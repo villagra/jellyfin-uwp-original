6cf28e1bdcb64f92fa041c80d550530060a8ad6a
Jose <villagra@gmail.com>
2020-03-22 22:36:21 +0100

    initial commit

---
+++ jellyfin-uwp/Controls/JellyfinWebView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace jellyfin_uwp.Controls
+{
+    public sealed partial class JellyfinWebView : UserControl
+    {
+        public JellyfinWebView()
+        {
+            this.InitializeComponent();
+
+            WView.ContainsFullScreenElementChanged += JellyfinWebView_ContainsFullScreenElementChanged;
+            WView.NavigationCompleted += JellyfinWebView_NavigationCompleted;
+
+            WView.Navigate(new Uri(SettingsStore.AppURL));
+        }
+
+        private async void JellyfinWebView_NavigationCompleted(Windows.UI.Xaml.Controls.WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            await WView.InvokeScriptAsync("eval", new string[] { "navigator.gamepadInputEmulation = 'gamepad';" });
+        }
+
+        private void JellyfinWebView_ContainsFullScreenElementChanged(Windows.UI.Xaml.Controls.WebView sender, object args)
+        {
+            ApplicationView appView = ApplicationView.GetForCurrentView();
+
+            if (sender.ContainsFullScreenElement)
+            {
+                appView.TryEnterFullScreenMode();
+                return;
+            }
+
+            if (!appView.IsFullScreenMode)
+            {
+                return;
+            }
+
+            appView.ExitFullScreenMode();
+        }
+    }
+}
---
+++ jellyfin-uwp/SettingsStore.cs
@@ -0,0 +1,23 @@
+﻿using Windows.Storage;
+
+namespace jellyfin_uwp
+{
+    public static class SettingsStore
+    {
+        public static string AppURL {
+            get => "https://demo.jellyfin.org/stable"; //(string)(_localSettings.Values["settings.app.AppURL"] ?? string.Empty);
+            set => ApplicationData.Current.LocalSettings.Values["settings.app.AppURL"] = value;
+        }
+
+        public static bool AppURLValid {
+            get => true; //(bool)(_localSettings.Values["settings.app.AppURLValid"] ?? false);
+            set => ApplicationData.Current.LocalSettings.Values["settings.app.AppURLValid"] = value;
+        }
+
+        public static bool AppURLIsJellyfin {
+            get => true; //(bool)(_localSettings.Values["settings.app.AppURLIsJellyfin"] ?? false);
+            set => ApplicationData.Current.LocalSettings.Values["settings.app.AppURLIsJellyfin"] = value;
+        }
+
+    }
+}
